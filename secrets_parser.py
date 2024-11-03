a1d1501726f03f22cb1648ae298ccf8f7ea05cba
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2024-11-03 23:34:05 +0530

    updated secrets_parser to json loads environment string variables

--- secrets_parser.py
+++ secrets_parser.py
@@ -8,7 +8,13 @@ def parse(path):
             x=x.strip()
             if "=" in x:
                 splitted_x=x.split("=", 1)
-                env[splitted_x[0]]=json.loads(splitted_x[1])
+                env[splitted_x[0]]=splitted_x[1]
     except:
         pass
-    return env | dict(os.environ)
+    env=env | dict(os.environ)
+    for x in env:
+        try:
+            env[x]=json.loads(env[x])
+        except:
+            pass
+    return 
